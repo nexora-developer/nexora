d2517c7ac42bcbc78e9a2d6ea606710abe250464
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-07 18:23:32 +0530

    Add Product page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,6 @@
 import React from "react";
-import products from "../data/products"; // Ensure products.js exist
+import products from "../data/products";
+import ProductCard from "../components/ProductCard";
 
 const Products = () => {
   return (
@@ -7,11 +8,7 @@ const Products = () => {
       <h1>Products</h1>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {products.map((product) => (
-          <div key={product.id} style={{ margin: "10px", border: "1px solid gray", padding: "10px" }}>
-            <img src={product.image} alt={product.name} width="150" />
-            <h3>{product.name}</h3>
-            <p>₹{product.price}</p>
-          </div>
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
