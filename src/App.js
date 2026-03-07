a09b2997e0011daa122bf33ec47151dc1c460e96
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-07 21:14:46 +0530

    Add App.js with CartProvider and Products

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { CartProvider } from "./context/CartContext";
 import Products from "./pages/Products";
-import Checkout from "./pages/Checkout";
-import Shops from "./pages/Shops";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/shops" element={<Shops />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
+        <h1 style={{ textAlign: "center", color: "#FF6600" }}>NEXORA E-Commerce</h1>
+        <Products />
+      </div>
+    </CartProvider>
   );
 }
 
