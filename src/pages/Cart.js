8bb77ca5090b9d0c5572ef3e99cbb4b33fd27212
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-07 15:08:51 +0530

    add cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,27 +1,12 @@
-import React, { useContext } from "react";
-import { CartContext } from "../components/CartContext";
+import React from "react";
 
-function Cart(){
-
-  const { cart } = useContext(CartContext);
-
-  return(
+function Cart() {
+  return (
     <div>
-
       <h2>Your Cart</h2>
-
-      {cart.length === 0 ? (
-        <p>No items in cart</p>
-      ) : (
-        cart.map((item,index)=>(
-          <div key={index}>
-            {item.name} - ₹{item.price}
-          </div>
-        ))
-      )}
-
+      <p>Cart items will appear here</p>
     </div>
-  )
+  );
 }
 
 export default Cart;
