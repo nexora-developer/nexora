9c08efd162c12a5616e3db1a3deda69ad7aa9ebe
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-07 15:11:34 +0530

    added shop page

---
+++ src/pages/Shop.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Map from "../components/Map";
+
+function Shops() {
+  return (
+    <div>
+      <h2>Nearby Shops</h2>
+      <Map />
+    </div>
+  );
+}
+
+export default Shops;
