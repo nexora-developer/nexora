2a371f1ba72b6d0997c1af0714f023527f8484e4
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-07 17:30:16 +0530

    Created App.js with routing and CartProvider

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Cart from "./pages/Cart";
 import Shops from "./pages/Shops";
+import GrainExchange from "./pages/GrainExchange";
+import GasBooking from "./pages/GasBooking";
+import Checkout from "./pages/Checkout";
+import AIPage from "./pages/AIPage";
+
+import { CartProvider } from "./context/CartContext";
 
 function App() {
-
   return (
-    <Router>
-
-      <div style={{ padding: "20px", fontFamily: "Arial" }}>
-
-        <h1>Nexora Marketplace</h1>
-
-        <nav style={{ marginBottom: "20px" }}>
-
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/products">Products</Link> |{" "}
-          <Link to="/cart">Cart</Link> |{" "}
-          <Link to="/shops">Shops</Link>
-
-        </nav>
-
+    <CartProvider>
+      <Router>
         <Routes>
-
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
           <Route path="/shops" element={<Shops />} />
-
+          <Route path="/grain-exchange" element={<GrainExchange />} />
+          <Route path="/gas-booking" element={<GasBooking />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/ai" element={<AIPage />} />
         </Routes>
-
-      </div>
-
-    </Router>
+      </Router>
+    </CartProvider>
   );
 }
 
