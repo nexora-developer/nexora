325d17aef51569cfca6cc9c072afcaa35d9c7c0c
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-07 19:04:59 +0530

    Add Product cards

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,16 +2,31 @@ import React from "react";
 
 const ProductCard = ({ product }) => {
   return (
-    <div style={{
-      border: 2px solid ${product.color},
-      borderRadius: "8px",
-      padding: "10px",
-      width: "200px"
-    }}>
-      <img src={product.image} alt={product.name} style={{ width: "100%", height: "150px", objectFit: "cover" }} />
+    <div
+      style={{
+        border: 2px solid ${product.color},
+        borderRadius: "8px",
+        padding: "10px",
+        width: "200px"
+      }}
+    >
+      <img
+        src={product.image}
+        alt={product.name}
+        style={{ width: "100%", height: "150px", objectFit: "cover" }}
+      />
       <h3>{product.name}</h3>
       <p>₹{product.price}</p>
-      <button style={{ padding: "5px 10px", backgroundColor: product.color, color: "white", border: "none", borderRadius: "4px", cursor: "pointer" }}>
+      <button
+        style={{
+          padding: "5px 10px",
+          backgroundColor: product.color,
+          color: "white",
+          border: "none",
+          borderRadius: "4px",
+          cursor: "pointer"
+        }}
+      >
         Add to Cart
       </button>
     </div>
