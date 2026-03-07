d7994d98a1b9ad28010a4910d56742bf8b1df6e4
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-07 14:48:00 +0530

    added google map shops

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,11 +11,39 @@ const center = {
   lng: 80.9462
 };
 
+const shops = [
+  {
+    name: "Sharma Store",
+    lat: 26.8500,
+    lng: 80.9500
+  },
+  {
+    name: "Gupta Shop",
+    lat: 26.8400,
+    lng: 80.9400
+  },
+  {
+    name: "Khan Mart",
+    lat: 26.8450,
+    lng: 80.9550
+  }
+];
+
 function Map() {
   return (
     <LoadScript googleMapsApiKey="AIzaSyAPoibhxlLUpR_cU8Qmt9sZrKWIExEbitk">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-        <Marker position={center} />
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={13}
+      >
+        {shops.map((shop, index) => (
+          <Marker
+            key={index}
+            position={{ lat: shop.lat, lng: shop.lng }}
+            title={shop.name}
+          />
+        ))}
       </GoogleMap>
     </LoadScript>
   );
