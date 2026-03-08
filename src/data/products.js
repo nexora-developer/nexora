572c4c25821d9673201d677068194008a8167ee4
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 13:56:12 +0530

    save

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,6 @@ const products = [
 id:1,
 name:"Tomato",
 price:30,
-category:"vegetables",
 image:"https://images.unsplash.com/photo-1561136594-7f68413baa99"
 },
 
@@ -12,7 +11,6 @@ image:"https://images.unsplash.com/photo-1561136594-7f68413baa99"
 id:2,
 name:"Potato",
 price:25,
-category:"vegetables",
 image:"https://images.unsplash.com/photo-1518977676601-b53f82aba655"
 },
 
@@ -20,7 +18,6 @@ image:"https://images.unsplash.com/photo-1518977676601-b53f82aba655"
 id:3,
 name:"Milk",
 price:55,
-category:"dairy",
 image:"https://images.unsplash.com/photo-1582719478250-c89cae4dc85b"
 },
 
@@ -28,7 +25,6 @@ image:"https://images.unsplash.com/photo-1582719478250-c89cae4dc85b"
 id:4,
 name:"Rice",
 price:80,
-category:"grocery",
 image:"https://images.unsplash.com/photo-1586201375761-83865001e31c"
 },
 
@@ -36,16 +32,7 @@ image:"https://images.unsplash.com/photo-1586201375761-83865001e31c"
 id:5,
 name:"Gas Cylinder",
 price:1100,
-category:"gas",
 image:"https://images.unsplash.com/photo-1603398938378-e54eab446dde"
-},
-
-{
-id:6,
-name:"Chocolate Cake",
-price:450,
-category:"sweets",
-image:"https://images.unsplash.com/photo-1605478907713-5b0c82a2f21b"
 }
 
 ]
