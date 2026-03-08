db654950b89958480ac7bb314747a261c51e1849
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 11:15:24 +0530

    save

--- src/data/products.js
+++ src/data/products.js
@@ -1,11 +1,9 @@
-const products = Array.from({length:29},(_,i)=>({
+const products = Array.from({ length: 29 }, (_, i) => ({
+  id: i + 1,
+  name: "Product " + (i + 1),
+  price: 100 + i * 20,
+  category: i % 2 === 0 ? "Grocery" : "Electronics",
+  image: "https://via.placeholder.com/200"
+}));
 
-id:i+1,
-name:"Product "+(i+1),
-price:100+i*15,
-category:i%2===0?"Grocery":"Electronics",
-image:"https://via.placeholder.com/200"
-
-}))
-
-export default products
+export default products;
