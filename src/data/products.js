9cafba8cf0431a0474ce84938fb7561e2623cada
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 11:29:25 +0530

    save

--- src/data/products.js
+++ src/data/products.js
@@ -1,9 +1,85 @@
-const products = Array.from({ length: 29 }, (_, i) => ({
-  id: i + 1,
-  name: "Product " + (i + 1),
-  price: 100 + i * 20,
-  category: i % 2 === 0 ? "Grocery" : "Electronics",
-  image: "https://via.placeholder.com/200"
-}));
+const products = [
 
-export default products;
+{
+id:1,
+name:"Tomato",
+price:30,
+category:"Vegetables",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:2,
+name:"Potato",
+price:25,
+category:"Vegetables",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:3,
+name:"Milk",
+price:55,
+category:"Dairy",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:4,
+name:"Paneer",
+price:300,
+category:"Dairy",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:5,
+name:"Rice",
+price:80,
+category:"Grocery",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:6,
+name:"Sugar",
+price:45,
+category:"Grocery",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:7,
+name:"Chocolate Cake",
+price:450,
+category:"Cakes",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:8,
+name:"Rasgulla",
+price:200,
+category:"Sweets",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:9,
+name:"Gas Cylinder",
+price:1100,
+category:"Gas",
+image:"https://via.placeholder.com/200"
+},
+
+{
+id:10,
+name:"Cooking Oil",
+price:180,
+category:"Grocery",
+image:"https://via.placeholder.com/200"
+}
+
+]
+
+export default products
