f26cfe6ef73eeadc7fc3deae68fa9a04b7cbcee0
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 12:01:10 +0530

    save

---
+++ src/pages/GasBooking.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function GasBooking(){
+
+return(
+
+<div style={{padding:"40px"}}>
+
+<h2>Gas Cylinder Booking</h2>
+
+<p>Book your cylinder here</p>
+
+<button>Book Now</button>
+
+</div>
+
+)
+
+}
+
+export default GasBooking
