2af9dc306f11b05d368ac3e47d086192af66acac
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 13:25:14 +0530

    save

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,16 +3,26 @@ import {CartContext} from "../context/CartContext"
 
 function ProductCard({product}){
 
-const {addToCart}=useContext(CartContext)
+const {addToCart} = useContext(CartContext)
 
 return(
 
 <div style={{
+
 border:"1px solid #ddd",
-padding:"20px",
 borderRadius:"10px",
-background:"#f8fafc"
+padding:"15px",
+textAlign:"center",
+background:"white",
+boxShadow:"0 2px 8px rgba(0,0,0,0.1)"
+
 }}>
+
+<img
+src={product.image}
+alt={product.name}
+style={{width:"100%",height:"150px",objectFit:"cover"}}
+/>
 
 <h3>{product.name}</h3>
 
@@ -21,15 +31,13 @@ background:"#f8fafc"
 <button
 onClick={()=>addToCart(product)}
 style={{
-background:"#22c55e",
-color:"white",
+background:"#ff9900",
 border:"none",
-padding:"10px"
+padding:"10px",
+cursor:"pointer"
 }}
->
-
-Add to Cart
-
+> 
+Addto Cart
 </button>
 
 </div>
