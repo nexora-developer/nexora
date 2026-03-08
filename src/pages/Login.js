c0c8b17121aafd73e8580334502d5c3fa0ce8e52
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 14:21:46 +0530

    save

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,61 @@
-localStorage.setItem("user",
-JSON.stringify({
+import React,{useState} from "react"
+
+function Login(){
+
+const [name,setName] = useState("")
+const [phone,setPhone] = useState("")
+
+function handleLogin(){
+
+if(!name || !phone){
+alert("Enter name and phone")
+return
+}
+
+const user = {
 name:name,
 phone:phone
-}))
+}
+
+localStorage.setItem("user",JSON.stringify(user))
+
+window.location.reload()
+
+}
+
+return(
+
+<div style={{
+padding:"40px",
+textAlign:"center"
+}}>
+
+<h2>Nexora Login</h2>
+
+<input
+placeholder="Enter Name"
+value={name}
+onChange={(e)=>setName(e.target.value)}
+/>
+
+<br/><br/>
+
+<input
+placeholder="Enter Phone"
+value={phone}
+onChange={(e)=>setPhone(e.target.value)}
+/>
+
+<br/><br/>
+
+<button onClick={handleLogin}>
+Login
+</button>
+
+</div>
+
+)
+
+}
+
+export default Login
