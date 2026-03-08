bdb57c093b84dd6f6686b1197330f63a93edf3c4
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 13:04:00 +0530

    save

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React from "react"
+import ProductCard from "../components/ProductCard"
 
 const products=[
+
 {name:"Tomato",price:30},
+
 {name:"Potato",price:25},
+
+{name:"Onion",price:40},
+
+{name:"Milk",price:60},
+
+{name:"Gas Cylinder",price:1100},
+
+{name:"Chocolate Cake",price:450},
+
 {name:"Rice",price:80},
-{name:"Milk",price:55},
-{name:"Cake",price:400}
+
+{name:"Sugar",price:45},
+
+{name:"Paneer",price:90}
+
 ]
 
 function Products(){
@@ -21,19 +36,7 @@ padding:"40px"
 
 {products.map((p,i)=>(
 
-<div key={i}
-style={{
-border:"1px solid #ddd",
-padding:"20px"
-}}>
-
-<h3>{p.name}</h3>
-
-<p>₹{p.price}</p>
-
-<button>Add to Cart</button>
-
-</div>
+<ProductCard key={i} product={p}/>
 
 ))}
 
