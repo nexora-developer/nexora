827f9edd98eeff121d874dd6bf5ff0ad500c2998
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 14:45:01 +0530

    save

--- src/data/products.js
+++ src/data/products.js
@@ -1,39 +1,14 @@
 const products = [
 
-{
-id:1,
-name:"Tomato",
-price:30,
-image:"https://images.unsplash.com/photo-1561136594-7f68413baa99"
-},
+{ id:1, name:"Tomato", price:30 },
 
-{
-id:2,
-name:"Potato",
-price:25,
-image:"https://images.unsplash.com/photo-1518977676601-b53f82aba655"
-},
+{ id:2, name:"Potato", price:20 },
 
-{
-id:3,
-name:"Milk",
-price:55,
-image:"https://images.unsplash.com/photo-1582719478250-c89cae4dc85b"
-},
+{ id:3, name:"Milk", price:60 },
 
-{
-id:4,
-name:"Rice",
-price:80,
-image:"https://images.unsplash.com/photo-1586201375761-83865001e31c"
-},
+{ id:4, name:"Rice", price:80 },
 
-{
-id:5,
-name:"Gas Cylinder",
-price:1100,
-image:"https://images.unsplash.com/photo-1603398938378-e54eab446dde"
-}
+{ id:5, name:"Gas Cylinder", price:1100 }
 
 ]
 
---
+++ src/pages/Map.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function Map(){
+
+return(
+
+<div style={{padding:"40px"}}>
+
+<h2>Nearby Shops Map</h2>
+
+<iframe
+title="map"
+width="100%"
+height="500"
+src="https://maps.google.com/maps?q=grocery store&t=&z=13&ie=UTF8&iwloc=&output=embed"
+/>
+
+</div>
+
+)
+
+}
+
+export default Map
