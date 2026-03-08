f034909563ab8928d7dbc1997addff3be23e35d7
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 11:05:04 +0530

    save

--- src/data/products.js
+++ src/data/products.js
@@ -2,7 +2,7 @@ const products = Array.from({length:29},(_,i)=>({
 
 id:i+1,
 name:"Product "+(i+1),
-price:100+i*20,
+price:100+i*15,
 category:i%2===0?"Grocery":"Electronics",
 image:"https://via.placeholder.com/200"
 
