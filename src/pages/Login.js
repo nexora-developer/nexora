9e21ba6c994b8606b3b25854f75deae3c23d2858
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 13:51:54 +0530

    save

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,42 +1,95 @@
 import React,{useState} from "react"
+import {auth} from "../firebase"
+
+import {
+signInWithPhoneNumber,
+RecaptchaVerifier
+} from "firebase/auth"
 
 function Login(){
 
-const[email,setEmail]=useState("")
-const[password,setPassword]=useState("")
+const [phone,setPhone] = useState("")
+const [name,setName] = useState("")
+const [otp,setOtp] = useState("")
+const [confirm,setConfirm] = useState(null)
 
-function handleLogin(){
+function sendOtp(){
 
-alert("Login Successful")
+window.recaptchaVerifier = new RecaptchaVerifier(
+"recaptcha",
+{size:"invisible"},
+auth
+)
+
+const appVerifier = window.recaptchaVerifier
+
+signInWithPhoneNumber(auth,phone,appVerifier)
+.then((result)=>{
+
+setConfirm(result)
+
+alert("OTP Sent")
+
+})
+
+}
+
+function verifyOtp(){
+
+confirm.confirm(otp)
+.then((result)=>{
+
+const user = result.user
+
+localStorage.setItem("user",
+JSON.stringify({
+name:name,
+phone:user.phoneNumber
+}))
+
+window.location="/"
+
+})
 
 }
 
 return(
 
-<div style={{padding:"40px"}}>
+<div style={{
+padding:"40px",
+maxWidth:"400px",
+margin:"auto"
+}}>
 
 <h2>Login</h2>
 
 <input
-placeholder="Email"
-onChange={(e)=>setEmail(e.target.value)}
+placeholder="Your Name"
+value={name}
+onChange={(e)=>setName(e.target.value)}
 />
-
-<br/><br/>
 
 <input
-type="password"
-placeholder="Password"
-onChange={(e)=>setPassword(e.target.value)}
+placeholder="Phone Number"
+value={phone}
+onChange={(e)=>setPhone(e.target.value)}
 />
 
-<br/><br/>
-
-<button onClick={handleLogin}>
-
-Login
-
+<button onClick={sendOtp}>
+Send OTP
 </button>
+
+<input
+placeholder="Enter OTP"
+value={otp}
+onChange={(e)=>setOtp(e.target.value)}
+/>
+
+<button onClick={verifyOtp}>
+Verify
+</button>
+
+<div id="recaptcha"></div>
 
 </div>
 
