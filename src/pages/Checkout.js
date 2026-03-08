d53cf33c2d4bc76680ec2c6fca2097256fa22474
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 13:28:39 +0530

    save

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React,{useContext} from "react"
+import {CartContext} from "../context/CartContext"
 
-function Checkout() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Checkout</h1>
-      <p>Order summary will appear here</p>
-    </div>
-  );
+function Checkout(){
+
+const {cart} = useContext(CartContext)
+
+const total = cart.reduce((sum,item)=>sum+item.price,0)
+
+return(
+
+<div style={{padding:"40px"}}>
+
+<h2>Checkout</h2>
+
+{cart.map((item,i)=>(
+
+<div key={i}>
+
+{item.name} - ₹{item.price}
+
+</div>
+
+))}
+
+<h3>Total: ₹{total}</h3>
+
+<button
+style={{
+background:"green",
+color:"white",
+padding:"10px",
+border:"none"
+}}
+> 
+PlaceOrder
+</button>
+
+</div>
+
+)
+
 }
 
-export default Checkout;
+export default Checkout
