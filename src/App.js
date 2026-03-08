87b31f7c2954d167d0d9f37c38115b326bd0c0c6
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-08 13:54:05 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from "react"
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-
-import Navbar from "./components/Navbar"
-
+import Login from "./pages/Login"
 import Home from "./pages/Home"
-import Products from "./pages/Products"
-import Cart from "./pages/Cart"
-import Checkout from "./pages/Checkout"
-import Orders from "./pages/Orders"
 
 function App(){
 
+const user = JSON.parse(localStorage.getItem("user"))
+
 return(
 
-<BrowserRouter>
+<div>
 
-<Navbar/>
+{user ? <Home/> : <Login/>}
 
-<Routes>
-
-<Route path="/" element={<Home/>}/>
-<Route path="/products" element={<Products/>}/>
-<Route path="/cart" element={<Cart/>}/>
-<Route path="/checkout" element={<Checkout/>}/>
-<Route path="/orders" element={<Orders/>}/>
-
-</Routes>
-
-</BrowserRouter>
+</div>
 
 )
 
