d778437f9b1f9be2ae16b58d3f48f6c0523cf166
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-09 14:40:04 +0530

    save

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,42 +3,25 @@ import {Link} from "react-router-dom"
 
 function Navbar(){
 
-const user = JSON.parse(localStorage.getItem("user"))
-
-function logout(){
-
-localStorage.removeItem("user")
-window.location="/"
-
-}
-
 return(
 
 <div style={{
-background:"#111",
-color:"white",
-padding:"15px",
 display:"flex",
-justifyContent:"space-between"
+justifyContent:"space-between",
+background:"#222",
+padding:"15px",
+color:"white"
 }}>
 
 <h2>Nexora</h2>
 
 <div>
 
-<Link to="/">Home</Link>
-<Link to="/products">Products</Link>
+<Link to="/">Products</Link>
+
 <Link to="/cart">Cart</Link>
-<Link to="/map">Shops Map</Link>
-</div>
 
-<div>
-
-<h3>Hello {user?.name}</h3>
-
-<button onClick={logout}>
-Logout
-</button>
+<Link to="/map">Map</Link>
 
 </div>
 
