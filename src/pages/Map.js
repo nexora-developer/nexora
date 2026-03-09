599fcc79ee7eb81e27991e1f5e05f962389d27e0
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-09 14:44:27 +0530

    save

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -6,13 +6,13 @@ return(
 
 <div style={{padding:"40px"}}>
 
-<h2>Nearby Shops Map</h2>
+<h2>Nearby Shops</h2>
 
 <iframe
 title="map"
 width="100%"
 height="500"
-src="https://maps.google.com/maps?q=grocery store&t=&z=13&ie=UTF8&iwloc=&output=embed"
+src="https://maps.google.com/maps?q=grocery&t=&z=13&ie=UTF8&iwloc=&output=embed"
 />
 
 </div>
