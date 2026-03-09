1fd311a94a05ca2fbe7e1ec9ffd8558d80bceae3
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-09 14:43:09 +0530

    save

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,6 @@
-import React,{useContext} from "react"
-import {CartContext} from "../context/CartContext"
+import React from "react"
 
 function Cart(){
-
-const {cart,increase,decrease} = useContext(CartContext)
-
-const total = cart.reduce((sum,i)=>sum+i.price*i.qty,0)
 
 return(
 
@@ -13,25 +8,7 @@ return(
 
 <h2>Your Cart</h2>
 
-{cart.map(item=>(
-
-<div key={item.id}>
-
-<h3>{item.name}</h3>
-
-<p>₹{item.price}</p>
-
-<button onClick={()=>decrease(item.id)}>-</button>
-
-<span>{item.qty}</span>
-
-<button onClick={()=>increase(item.id)}>+</button>
-
-</div>
-
-))}
-
-<h2>Total ₹{total}</h2>
+<p>No items yet</p>
 
 </div>
 
