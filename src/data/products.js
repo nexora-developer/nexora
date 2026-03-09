05165f687f104305cf53324a80a30641eac969e2
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-09 14:35:26 +0530

    save

--- src/data/products.js
+++ src/data/products.js
@@ -1,14 +1,39 @@
 const products = [
 
-{ id:1, name:"Tomato", price:30 },
+{
+id:1,
+name:"Tomato",
+price:30,
+image:"https://via.placeholder.com/150"
+},
 
-{ id:2, name:"Potato", price:20 },
+{
+id:2,
+name:"Potato",
+price:20,
+image:"https://via.placeholder.com/150"
+},
 
-{ id:3, name:"Milk", price:60 },
+{
+id:3,
+name:"Milk",
+price:60,
+image:"https://via.placeholder.com/150"
+},
 
-{ id:4, name:"Rice", price:80 },
+{
+id:4,
+name:"Cake",
+price:400,
+image:"https://via.placeholder.com/150"
+},
 
-{ id:5, name:"Gas Cylinder", price:1100 }
+{
+id:5,
+name:"Gas Cylinder",
+price:1100,
+image:"https://via.placeholder.com/150"
+}
 
 ]
 
