c756aa7c3a16e75bca2ae45bec12deb4c442f606
Saurabh Yadav <kamalyadav4555@gmail.com>
2026-03-09 14:55:39 +0530

    Nexora cart system added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,11 @@
-import React from "react"
+import React,{useContext} from "react"
+import {CartContext} from "../context/CartContext"
 
 function Cart(){
+
+const {cart,increase,decrease} = useContext(CartContext)
+
+const total = cart.reduce((sum,item)=>sum+item.price*item.qty,0)
 
 return(
 
@@ -8,7 +13,35 @@ return(
 
 <h2>Your Cart</h2>
 
-<p>No items yet</p>
+{cart.length===0 && <p>No items</p>}
+
+{cart.map(item=>(
+
+<div key={item.id} style={{
+border:"1px solid #ccc",
+padding:"20px",
+marginBottom:"10px"
+}}>
+
+<h3>{item.name}</h3>
+
+<p>₹{item.price}</p>
+
+<button onClick={()=>decrease(item.id)}>-</button>
+
+<span style={{margin:"10px"}}>
+
+{item.qty}
+
+</span>
+
+<button onClick={()=>increase(item.id)}>+</button>
+
+</div>
+
+))}
+
+<h2>Total ₹{total}</h2>
 
 </div>
 
